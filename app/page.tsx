900deec9159c639772514023f514acdfd436f98c
Kartik <hellokartikk@gmail.com>
2024-05-01 15:24:02 +0530

    Add Link component to page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import Image from "next/image";
 import Logo from "@/assets/logo.svg";
+import Link from "next/link";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -18,7 +19,7 @@ export default async function Index() {
   const isSupabaseConnected = canInitSupabaseClient();
 
   return (
-    <div className="flex flex-col items-center flex-1 w-full gap-20">
+    <div className="flex flex-col items-center flex-1 w-full h-screen gap-20">
       <nav className="flex justify-center w-full h-16 border-b border-border">
         <div className="flex items-center justify-between w-full max-w-4xl p-3 text-sm">
           <Image
@@ -35,6 +36,7 @@ export default async function Index() {
         <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
           Scripting Layer for <span className="font-bold"><br />no-code tools</span>
         </p>
+        <Link href={'/projects'} className="mx-auto text-green-700 underline">Go to app</Link>
       </div>
 
       <footer className="flex justify-center w-full p-8 text-xs text-center border-t border-t-foreground/10">
