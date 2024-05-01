f6524d9f4d739bff639710e63ac5e3cc5dc5c4db
Kartik <hellokartikk@gmail.com>
2024-05-01 15:51:32 +0530

    go back from editor page & enable toasts

--- app/(protected)/editor/page.tsx
+++ app/(protected)/editor/page.tsx
@@ -3,33 +3,35 @@ import Logo from "@/assets/logo.svg";
 import Editor from "@/components/Editor";
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
 
 async function EditorPage({
-    searchParams,
-  }: {
-    searchParams?: { projectId?: string | undefined };
-  }) {
+  searchParams,
+}: {
+  searchParams?: { projectId?: string | undefined };
+}) {
 
-    if(!searchParams?.projectId?.trim() || searchParams?.projectId === 'undefined') redirect('/projects')
+  if (!searchParams?.projectId?.trim() || searchParams?.projectId === 'undefined') redirect('/projects')
 
-    const supabase = createClient();
-    const files = await supabase.from('files')
-        .select('*')
-        .eq('project', searchParams?.projectId)
-    
+  const supabase = createClient();
+  const files = await supabase.from('files')
+    .select('*')
+    .eq('project', searchParams?.projectId)
 
   return (
     <div className="relative flex flex-col h-screen overflow-hidden">
       {/* header */}
       <div className="flex items-center justify-between p-4 border-b h-14 border-border">
-        <Image src={Logo} alt="ThunderBox" />
+        <Link href={'/projects'}>
+          <Image src={Logo} alt="ThunderBox" />
+        </Link>
 
         {/* <button className="flex text-sm items-center gap-2 p-4 h-9 hover:brightness-110 text-[#19B785] bg-[#1B3634] rounded-full">
           Push to production
         </button> */}
       </div>
 
-      <Editor 
+      <Editor
         projectId={searchParams?.projectId}
         files={files.data || []}
       />
